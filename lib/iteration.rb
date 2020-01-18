7620473b5b47a417666ffbb2a202fbeb46e8599e
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-18 21:51:13 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -41,6 +41,7 @@ end
     rowindex += 1
 
   end
+  p total
 end
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
